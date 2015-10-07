rohitporoli/platform_vendor_to

Initial Marshmallow PureNexus Vendor
*Added all devices (angler, bullhead, flo, flounder, hammerhead, shamu)
*Added overlay support
*Added basic AOSP build.prop fixs
*Added AOSP keyboard swype support & dictionaries
*Added basic purenexus versioning
*Added SIP+VoIP support
*Added AOSP/Google location provider config
*Build AOSP Keyboard, Launcher3 & libemoji
*Build stk & cellbradcast for telephony devices
*Enabled selinux in about phone

vendorsetup.sh
@@ -1,0 +1,6 @@
+add_lunch_combo nexus_anngler-user
+add_lunch_combo nexus_bullhead-user
+add_lunch_combo nexus_flo-user
+add_lunch_combo nexus_flounder-user
+add_lunch_combo nexus_hammerhead-user
+add_lunch_combo nexus_shamu-user

pranav01/vendor_radium

vendorsetup.sh
@@ -1,0 +1,6 @@
+add_lunch_combo nexus_angler-user
+add_lunch_combo nexus_bullhead-user
+add_lunch_combo nexus_flo-user
+add_lunch_combo nexus_flounder-user
+add_lunch_combo nexus_hammerhead-user
+add_lunch_combo nexus_shamu-user