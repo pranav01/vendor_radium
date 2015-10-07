8be9b592ce7d8d8f8ffa1211d63246f242088078
beanstown106 <nbenis106@gmail.com>
2015-10-07 18:47:22 -0400

    add stock marshmallow bootanimations for all current nexus's * we will decide on custom PureNexus animations later

--- main.mk
+++ main.mk
@@ -15,4 +15,5 @@
 include vendor/nexus/configs/aosp_fixes.mk
 include vendor/nexus/configs/system.mk
 include vendor/nexus/configs/nexus_build.mk
+include vendor/nexus/configs/bootanimation.mk
 include vendor/nexus/configs/version.mk
