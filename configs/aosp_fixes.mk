8d65428c54f5963e9126b568a9ad4ba39b9eae35
beanstown106 <nbenis106@gmail.com>
2015-10-11 02:47:14 -0400

    drop date format as it is no longer needed

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -20,7 +20,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
 # Enable SIP+VoIP
